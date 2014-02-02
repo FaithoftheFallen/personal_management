9a6e9f0ef45daac9b707c4f24aaa3bda0180579f
Semz <semz@semz-p6-2120-Linux.(none)>
2014-02-02 15:22:26 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PersonalManagement::Application.config.secret_token = 'd5703ea4773944f8a61224a4799a00f78320a107d46a74e9c2ba299d3de5f203c6416c637e17be2269237c7c8811a3efdff4f2d560e6e38de4f9e9214ad7261f'
